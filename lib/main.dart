f6a2a0a0db1c76fdb1d7fc7b059bad50fc57e7af
DexterTalha <talhaviet22@gmail.com>
2023-02-19 12:28:53 +0530

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:dexter_cart/screens/login_screen.dart';
 import 'package:dexter_cart/utils/my_routes.dart';
-import 'package:get/get.dart';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -19,15 +19,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp.router(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
       ),
-      key: Get.key,
-      routerDelegate: MyRoutes().router.routerDelegate,
-      routeInformationParser: MyRoutes().router.routeInformationParser,
-      routeInformationProvider: MyRoutes().router.routeInformationProvider,
+      // routerDelegate: MyRoutes().router.routerDelegate,
+      // routeInformationParser: MyRoutes().router.routeInformationParser,
+      routerConfig: MyRoutes().router,
     );
   }
 }
